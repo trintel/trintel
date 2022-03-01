5b5711b3d90be4725d59cbe194f895c4d4087b04
Luca Mayer <stu223151@mail.uni-kiel.de>
2022-03-01 11:46:43 +0000

    Init

---
+++ src/test/java/sopro/Main.java
@@ -0,0 +1,13 @@
+package sopro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Main {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
