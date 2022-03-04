a4a98648b1aa9a1daed7d83c2dfaa418e3d02c8d
b3z <b3z@zeppel.eu>
2022-03-04 13:54:28 +0100

    Fix Kommentare

--- src/test/java/sopro/DatabaseTest.java
+++ src/test/java/sopro/DatabaseTest.java
@@ -1,35 +1,17 @@
 package sopro;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.event.annotation.AfterTestMethod;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
-
 
 import sopro.model.Company;
 import sopro.model.User;
 import sopro.repository.CompanyRepository;
 import sopro.repository.UserRepository;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @Transactional
@@ -37,7 +19,7 @@ public class DatabaseTest {
 
     @Autowired
     UserRepository userRepository;
-    
+
     @Autowired
     CompanyRepository companyRepository;
 
@@ -49,32 +31,30 @@ public class DatabaseTest {
     String lastname = "lastname";
     String email = "email@email.com";
     String password = "password";
-    
-   
 
     /**
-     * Creates a User and a Company in the database
+     * Creates a User and a Company in the database.
+     * 
+     * @throws Exception
      */
     @BeforeEach
     public void addUserandCompany() {
         user = new User(lastname, forename, email, password, null);
-        user.setRole("STUDENT");                        //model -> user: Role not null
-        company= new Company("company");
+        user.setRole("STUDENT"); // model -> user: Role not null
+        company = new Company("company");
         companyRepository.save(company);
         user.setCompany(company);
         userRepository.save(user);
-        
-        }
+
+    }
 
     /**
      * Checks if a created user is really saved in database.
+     * 
+     * @throws Exception
      */
     @Test
-    public void isUserInDatabase() throws Exception{
-        assertEquals(user, userRepository.findById(user.getId()).get());        //.get(): optional<User> in User
+    public void isUserInDatabase() throws Exception {
+        assertEquals(user, userRepository.findById(user.getId()).get()); // .get(): optional<User> in User
     }
-
-
-
-    
 }
--- src/test/java/sopro/LoginTest.java
+++ src/test/java/sopro/LoginTest.java
@@ -1,30 +1,19 @@
 package sopro;
 
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.event.annotation.AfterTestMethod;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import sopro.model.User;
-import sopro.repository.UserRepository;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // @RunWith(SpringRunner.class)
@@ -32,12 +21,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class LoginTest {
 
-
     @Autowired
     private MockMvc mockMvc;
 
     /**
-     * Tests, if the login screen is served on /login
+     * Tests, if the login screen is served on /login.
+     * 
+     * @throws Exception
      */
     @Test
     public void loginAvailableForAll() throws Exception {
@@ -47,60 +37,68 @@ public class LoginTest {
     }
 
     /**
-     * @throws Exception 
-     * Tests if the Admnin can acces the companies
+     * Tests if the Admin can access the companies.
+     * 
+     * @throws Exception
      */
     @Test
-    @WithMockUser(username = "admin@admin", roles = {"ADMIN"}) // Erstellt User ohne die Daternbank zu verwenden
+    @WithMockUser(username = "admin@admin", roles = { "ADMIN" }) // Erstellt User ohne die Daternbank zu verwenden.
     public void adminCanSeeCompanies() throws Exception {
         mockMvc
                 .perform(get("/companies"))
                 .andExpect(status().isOk());
-        }
-    
+    }
+
     /**
-     * Test if the Logout of the Admin works
-     */    
+     * Test if the Logout of the Admin works.
+     */
     @Test
     @AfterTestMethod
     public void adminLogout() throws Exception {
-        mockMvc 
+        mockMvc
                 .perform(logout())
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/login?logout"));
     }
 
-     /**
-     * @throws Exception 
-     * Tests if the Student can acces the companies
+    /**
+     * Tests if the Student can access the companies.
+     * 
+     * @throws Exception
      */
     @Test
-    @WithMockUser(username = "student@student", roles = {"STUDENT"}) // Erstellt User ohne die Daternbank zu verwenden
+    @WithMockUser(username = "student@student", roles = { "STUDENT" }) // Erstellt User ohne die Daternbank zu
+                                                                       // verwenden.
     public void studentCanNotSeeCompanies() throws Exception {
         mockMvc
                 .perform(get("/companies"))
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/company/select"));
-        }
+    }
+
     /**
-     * Test if the Logout of the Student works
-     */    
+     * Test if the Logout of the Student works.
+     * 
+     * @throws Exception
+     */
     @Test
     @AfterTestMethod
     public void studentLogout() throws Exception {
-        mockMvc 
+        mockMvc
                 .perform(logout())
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/login?logout"));
     }
 
-
-    /** 
-     *  Tests, if the user is redirected to login error page when he uses wrong password
+    /**
+     * Tests, if the user is redirected to login error page when he uses wrong
+     * password.
+     * 
+     * @throws Exception
      */
     @Test
     public void testInvalidLoginDenied() throws Exception {
-        String loginErrorUrl = "/login?error";             
+        String loginErrorUrl = "/login?error";
         mockMvc
                 .perform(formLogin().password("invalid"))
                 .andExpect(status().isFound())
@@ -109,13 +107,16 @@ public class LoginTest {
 
         mockMvc
                 .perform(get(loginErrorUrl))
-                .andExpect(content().string(containsString("Invalid username and password")));
+                .andExpect(content().string(containsString("Invalid username and password"))); // TODO überarbeiten.
+                                                                                               // !Language spezifisch!
     }
 
     /**
      * Tests, if the client is redirected to login when accessing /companies as a
      * not authed user.
      * Code 302 = Redirection to another page
+     * 
+     * @throws Exception
      */
     @Test
     public void testRedirectToLogin() throws Exception {
@@ -126,29 +127,27 @@ public class LoginTest {
                 .getRedirectedUrl().endsWith("/login"));
     }
 
-
     /**
+     * Test if the Sign Up page is reachable for the Students.
+     * 
      * @throws Exception
-     * Test if the Sign Up page is reachable for the Students
      */
     @Test
-    public void testSignupPageForStudents() throws Exception{
+    public void testSignupPageForStudents() throws Exception {
         mockMvc
                 .perform(get("/signup/student"))
                 .andExpect(status().isOk());
     }
-    
+
     /**
+     * Test if the Sign Up page is reachable for the Admins.
+     * 
      * @throws Exception
-     * Test if the Sign Up page is reachable for the Admins
      */
     @Test
-    public void testSignupPageForAdmin() throws Exception{
+    public void testSignupPageForAdmin() throws Exception {
         mockMvc
                 .perform(get("/signup/admin"))
                 .andExpect(status().isOk());
     }
-
-    
-
 }
