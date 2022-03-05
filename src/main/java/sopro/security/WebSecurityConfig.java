6a7c802bc13c6c55192291b0b1c25e016f67701b
stu223495 <stu223495@mail.uni-kiel.de>
2022-03-05 12:08:40 +0100

    fixed logout and home redirection

--- src/main/java/sopro/security/WebSecurityConfig.java
+++ src/main/java/sopro/security/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/console/**", "/signup/**","/webjars/**", "/css/*", "/img/*", "/login/**").permitAll() // permit all to access those Mathes
+				.antMatchers("/console/**", "/signup/**","/webjars/**", "/css/*", "/img/*", "/login/**").permitAll() // permit all to access those Mathes
 				.antMatchers("/companies/**").hasRole("ADMIN") // restrict to only ADMIN role is able to access /companies/* 
 				.antMatchers("/company/**").hasRole("STUDENT") // restrict to only STUDENT role is able to access /company
 
