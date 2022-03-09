b1e06764118c571d237a0dcc41126ccbbd0cccdb
Christopher Werl <stu223495@mail.uni-kiel.de>
2022-03-09 23:35:32 +0000

    Improved company profile frontend

---
+++ src/main/java/sopro/repository/CompanyLogoRepository.java
@@ -0,0 +1,10 @@
+package sopro.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import sopro.model.Company;
+import sopro.model.CompanyLogo;
+
+public interface CompanyLogoRepository  extends CrudRepository<CompanyLogo, Long> {
+    CompanyLogo findByCompany(Company company);
+}
