1bd75650494efb08be7407eef5762aeda7783212
b3z <b3z@zeppel.eu>
2022-10-16 22:14:59 +0200

    remove unused import

--- src/main/java/sopro/TrintelApplication.java
+++ src/main/java/sopro/TrintelApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import sopro.service.PdfInterface;
 import sopro.service.SignupUrlInterface;
