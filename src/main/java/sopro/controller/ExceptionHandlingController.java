937b192a222d01e72e2d6662154ecbf5ac39a176
Felix Ohnesorge <felix-eutin@gmx.de>
2022-10-17 15:38:35 +0200

    Exception Handling

---
+++ src/main/java/sopro/controller/ExceptionHandlingController.java
@@ -0,0 +1,43 @@
+package sopro.controller;
+
+import java.sql.Timestamp;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.NestedServletException;
+
+import sopro.TrintelApplication;
+
+@ControllerAdvice
+public class ExceptionHandlingController {
+
+  // Exception handling methods
+
+
+  // Specify name of a specific view that will be used to display the error:
+  @ExceptionHandler({MaxUploadSizeExceededException.class, NestedServletException.class, SizeLimitExceededException.class})
+  public String databaseError(Model model) {
+    TrintelApplication.logger.error("File upload too large");
+    model.addAttribute("timestamp", new Timestamp(System.currentTimeMillis()));
+
+    return "error-file-size";
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+    TrintelApplication.logger.error("Request: " + req.getRequestURL() + " raised " + ex);
+
+    ModelAndView mav = new ModelAndView();
+    mav.addObject("exception", ex);
+    mav.addObject("url", req.getRequestURL());
+    mav.setViewName("error");
+    return mav;
+  }
+
+}
