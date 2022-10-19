000209663df2890a5039cc6f13a58b4a9b1974f3
cwerl <cwerl@outlook.de>
2022-10-19 21:24:59 +0200

    spotlessapply

--- src/main/java/sopro/controller/ExceptionHandlingController.java
+++ src/main/java/sopro/controller/ExceptionHandlingController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.NestedServletException;
 
 import sopro.TrintelApplication;
